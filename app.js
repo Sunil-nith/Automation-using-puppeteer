21ace987e7d71421f8c18bae070f0f64dd4a6752
Sunil Kumar <skjnv2009@gmail.com>
2023-07-16 19:38:03 +0530

    adding function to set chain value

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ const puppeteer = require('puppeteer');
     });
   const page = await browser.newPage();
   await page.goto('https://swap.defillama.com');
+
+  const setChain = async () => {
+    try {
+      await page.waitForSelector('#react-select-2-input');
+      await page.type('#react-select-2-input', 'Arbitrum');
+      await page.keyboard.press('Enter');
+    } catch (error) {
+      console.error('Error occurred during setChain:', error.message);
+    }
+  };
+
+  await setChain();
+
+  
+// Keep the browser window open
 })();
